12d575edaac49de9ebbf737452306052552ed58d
Logan McAnsh <logan@mcan.sh>
2021-11-03 13:30:55 -0400

    feat: add more version utilities

--- __tests__/get-versions.test.ts
+++ __tests__/get-versions.test.ts
@@ -1,4 +1,41 @@
-import { getVersions } from "../src";
+import {
+  getBranchFromRef,
+  getBranchOrTagFromRef,
+  getTagFromRef,
+  getVersionHead,
+  getVersions,
+} from "../src";
+
+it("returns the branch or tag", () => {
+  expect(getBranchOrTagFromRef("refs/heads/master")).toBe("master");
+  expect(getBranchOrTagFromRef("refs/tags/v1.0.0")).toBe("v1.0.0");
+  expect(getBranchOrTagFromRef("refs/tags/v1.0.0-beta.6")).toBe(
+    "v1.0.0-beta.6"
+  );
+  expect(getBranchOrTagFromRef("something")).toBe("something");
+});
+it("returns a branch", () => {
+  expect(getBranchFromRef("refs/heads/master")).toBe("master");
+  expect(getBranchFromRef("refs/tags/v1.0.0")).toBe("refs/tags/v1.0.0");
+  expect(getBranchFromRef("refs/tags/v1.0.0-beta.6")).toBe(
+    "refs/tags/v1.0.0-beta.6"
+  );
+  expect(getBranchFromRef("something")).toBe("something");
+});
+it("returns a tag", () => {
+  expect(getTagFromRef("refs/tags/v1.0.0")).toBe("v1.0.0");
+  expect(getTagFromRef("refs/tags/v1.0.0-beta.6")).toBe("v1.0.0-beta.6");
+  expect(getTagFromRef("refs/heads/master")).toBe("refs/heads/master");
+  expect(getTagFromRef("something")).toBe("something");
+});
+
+it("returns a version head", () => {
+  expect(() => getVersionHead("refs/heads/master")).toThrowError();
+  expect(getVersionHead("refs/tags/v0.0.1")).toBe("v0.0.1");
+  expect(getVersionHead("refs/tags/v0.0.1-beta.6")).toBe("v0.0.1-beta.6");
+  expect(getVersionHead("refs/tags/v0.1.0")).toBe("v0.1");
+  expect(getVersionHead("refs/tags/v1.0.0")).toBe("v1");
+});
 
 it("returns a VersionHead for each version", () => {
   const versions = getVersions([
--- src/lib/get-versions.ts
+++ src/lib/get-versions.ts
@@ -17,27 +17,48 @@ interface VersionHead {
   isLatest: boolean;
 }
 
+let branchOrTagRegex = /^refs\/(heads|tags)\//;
+let branchRegex = /^refs\/heads\//;
+let tagRegex = /^refs\/tags\//;
+
+function getBranchOrTagFromRef(ref: string): string {
+  return ref.replace(branchOrTagRegex, "");
+}
+
+function getBranchFromRef(ref: string): string {
+  return ref.replace(branchRegex, "");
+}
+function getTagFromRef(ref: string): string {
+  return ref.replace(tagRegex, "");
+}
+
+function getVersionHead(ref: string): string {
+  let version = getTagFromRef(ref);
+
+  let tag = semver.coerce(version);
+
+  if (!tag) {
+    throw new Error(`Invalid version: "${version}"`);
+  }
+
+  let head =
+    tag.major > 0
+      ? `v${tag.major}`
+      : tag.minor > 0
+      ? `v0.${tag.minor}`
+      : `v0.0.${tag.patch}`;
+
+  return head;
+}
+
 function getVersions(refs: Array<string>): Array<VersionHead> {
   let tags = refs.map((ref) => ref.replace(/^refs\/tags\//, ""));
-  let validTags = tags.filter((ref) =>
-    semver.valid(ref.replace(/^refs\/tags\//, ""))
-  );
+  let validTags = tags.filter((ref) => semver.valid(ref));
 
   let sorted = validTags.sort((a, b) => semver.compare(b, a));
 
   let versions = sorted.map((version) => {
-    let tag = semver.coerce(version);
-
-    if (!tag) {
-      throw new Error(`Invalid version: ${version}`);
-    }
-
-    let head =
-      tag.major > 0
-        ? `v${tag.major}`
-        : tag.minor > 0
-        ? `v0.${tag.minor}`
-        : `v0.0.${tag.patch}`;
+    let head = getVersionHead(version);
 
     return {
       head,
@@ -51,5 +72,11 @@ function getVersions(refs: Array<string>): Array<VersionHead> {
   return versions;
 }
 
-export { getVersions };
+export {
+  getVersions,
+  getVersionHead,
+  getBranchOrTagFromRef,
+  getBranchFromRef,
+  getTagFromRef,
+};
 export type { VersionHead };
